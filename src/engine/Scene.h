f73c09d57c0c4e87fd446f1c45e84a64210014e0
Naama <shlittna@post.bgu.ac.il>
2025-12-25 12:56:37 +0200

    wip

---
+++ src/engine/Scene.h
@@ -0,0 +1,21 @@
+#include <vector>
+#include <memory>
+#include "Camera.h"
+
+
+class Object3D;
+class Light;
+class Camera;
+
+class Scene
+{
+private:
+    /* data */
+public:
+    std::vector<std::shared_ptr<Object3D>> objs;
+    std::vector<std::shared_ptr<Light>> lights;
+    glm::vec3 ambientLight;   // Ambient intensity (r,g,b)
+    Camera cam;
+    Scene(/* args */);
+};
+
